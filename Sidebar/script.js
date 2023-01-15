d1a8caa55e39e6077f48fff521f3943a141b9c6c
Gabriel Barbosa <59917720+gabrielb-ss@users.noreply.github.com>
2023-01-15 12:56:02 -0300

    fix: fix side menu starting position and animation

--- Sidebar/script.js
+++ Sidebar/script.js
@@ -1,50 +1,13 @@
-const paragraphs = document.querySelectorAll('p')
-const logo= document.querySelector("#logo-menu")
 const nav = document.querySelector('nav')
 
-let menuIsHidden = false
+let menuIsHidden = true
 
 function toggleMenu () {
   if (menuIsHidden) {
+    nav.classList.add("expand")
     menuIsHidden = false
-    logo.classList.remove("hide-logo")
-    paragraphs.forEach(paragraph => {
-      paragraph.classList.remove('hide')
-      paragraph.classList.add('show')
-    })
-    nav.classList.remove("shrink")
   } else {
+    nav.classList.remove("expand")
     menuIsHidden = true
-    logo.classList.add("hide-logo")
-    paragraphs.forEach(paragraph => {
-      paragraph.classList.remove('show')
-      paragraph.classList.add('hide')
-    })
-    nav.classList.add("shrink")
   }
 }
-
-
-
-/*if(!logo.classList.contains("hide")) {
-  logo.classList.add("hide", "hide-logo")
-  paragraphs.forEach(paragraph => {
-    if(paragraph.classList.contains('show')) {
-      paragraph.classList.remove('show')
-    }
-    paragraph.classList.add('hide')
-  })
-  nav.classList.add("shrink")
-} else {
-  logo.classList.remove("hide-logo")
-  paragraphs.forEach(paragraph => { 
-    if(paragraph.classList.contains('hide')) {
-      paragraph.classList.remove("hide", "hide-logo")
-    }
-    paragraph.classList.add('show') 
- 
-    
-  })
-  nav.classList.remove("shrink")
-}
-*/
